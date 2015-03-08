24901723e3f83011a76ed3277060ba81726f7630
vnitnitya <nityakumari93@gmail.com>
2015-03-08 18:39:29 +0530

    server updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ app     = express(),
 bodyParser=require('body-parser'),
 	mongoose=require('mongoose'),
 meetupsController=require('./server/controllers/meetups-controller');
-mongoose.connect('mongodb://localhost:27017/mean-demo');
+mongoose.connect('mongodb://abc:1234@ds061548.mongolab.com:61548/mean-demo6');
 app.use(bodyParser());
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/client/views/index.html');
@@ -11,6 +11,5 @@ app.get('/',function(req,res){
 app.use('/js',express.static(__dirname+'/client/js'));
 app.get('/api/meetups',meetupsController.list);
 app.post('/api/meetups',meetupsController.create);
-app.listen(3000,function(){
-    console.log('I\' m Listening....');
-})
+app.listen(process.env.PORT || 5000);
+console.log("Server running on port no. 5000");
